ba6f787b55d916a9e8abb8847ac9e83f9f7b7b9a
marwenB <marwen.belghith@gmail.com>
2013-02-21 18:11:40 +0100

    fixing errors

--- db/migrate/20130221145607_create_products.rb
+++ db/migrate/20130221145607_create_products.rb
@@ -1,19 +1,20 @@
-# encoding: utf-8 
 class CreateProducts < ActiveRecord::Migration
   def change
-    create_table :products do |t| 
+    create_table :products do |t|
+      
       t.string :name, :null => false  
       t.string :refname 
       t.date :released_at 
       t.text :long_desc 
       t.integer :manufacturer_id 
       t.integer :cities_picture_id 
-      t.integer :product_id 
-      t.string :slug, :null => false  
-      t.timestamps 
+      t.string :slug, :null => false 
+      t.integer :brag_id
+      t.integer :original_product_id
+
+      t.timestamps
     end
     add_index(:products, [:cities_picture_id], :name => 'products_fk_product_user1_idx') 
     add_index(:products, [:product_id], :name => 'products_fk_product_product1_idx') 
   end
 end
-
